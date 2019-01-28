42ed493a45dd89d15f10696d00203abb94d1e35d
Stanik Sylvain <sylvain.stanik@etu.univ-grenoble-alpes.fr>
2019-01-28 11:00:09 +0000

    Ca change encore

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,21 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LuckyController extends Controller
+{
+    public function number($name, $job)
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+            'name' => $name,
+            'job' => $job,
+        ));
+    }
+}
+
